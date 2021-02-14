b62228cfb0fa26846ebc4c2da512129196d65579
timickb <timikm2002@gmail.com>
2021-02-14 14:33:03 +0300

    hmm... changes 4

--- TaskManagerCLI/TaskManager.Tasks.cs
+++ TaskManagerCLI/TaskManager.Tasks.cs
@@ -1,3 +1,6 @@
+using System;
+using TaskManagerLib;
+
 namespace TaskManagerCLI
 {
     /// <summary>
@@ -6,6 +9,46 @@ namespace TaskManagerCLI
     /// </summary>
     public partial class TaskManager
     {
-        
+        /// <summary>
+        /// Assigns the user to the task.
+        /// </summary>
+        /// <param name="user">User object</param>
+        /// <param name="task">Taks object</param>
+        /// <exception cref="ArgumentException">Exception throws than the specified
+        /// task object is not IAssignable</exception>
+        /// <exception cref="InvalidOperationException">Exception throws than the specified
+        /// task doesn't allow to assign any more users to itself.</exception>
+        public void AssignUserToTask(User user, Task task)
+        {
+            if (!(task is IAssignable))
+            {
+                throw new ArgumentException("Task object must be IAssignable.");
+            }
+
+            try
+            {
+                ((IAssignable) task).AddExecutor(user);
+            }
+            catch (AssigningException)
+            {
+                throw new InvalidOperationException("Unable to assign any more executors for specified task.");
+            }
+        }
+
+        /// <summary>
+        /// Removes the user assigment to the task.
+        /// </summary>
+        /// <param name="user">User object</param>
+        /// <param name="task">Task object</param>
+        /// <exception cref="ArgumentException">Exception throws than the specified
+        /// task object is not IAssignable</exception>
+        public void RemoveAssignedUserFromTask(User user, Task task)
+        {
+            if (!(task is IAssignable))
+            {
+                throw new ArgumentException("Task object must be IAssignable.");
+            }
+            ((IAssignable) task).RemoveExecutor(user);
+        }
     }
 }
