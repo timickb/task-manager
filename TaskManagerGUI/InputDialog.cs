5b4aab6b15b4faef751ba14291bc9cd64e047cdd
timickb <timikm2002@gmail.com>
2021-02-23 11:13:44 +0300

    GUI

---
+++ TaskManagerGUI/InputDialog.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TaskManagerGUI
+{
+    public partial class InputDialog : Form
+    {
+        public string Value { get; private set; }
+        
+        public InputDialog(string text)
+        {
+            InitializeComponent();
+            this.Text = text;
+        }
+
+        public sealed override string Text
+        {
+            get => base.Text;
+            set => base.Text = value;
+        }
+
+        private void applyButton_Click(object sender, EventArgs e)
+        {
+            Value = textBox1.Text;
+            DialogResult = DialogResult.OK;
+            Close();
+        }
+    }
+}
